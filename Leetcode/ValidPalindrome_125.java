a90c4f9c56c2292e64d40d1429f665b28093fd51
Raviprasaath <raviprasaath1@gmail.com>
2023-03-10 18:25:48 +0530

    updated

---
+++ Leetcode/ValidPalindrome_125.java
@@ -0,0 +1,36 @@
+package Leetcode;
+
+public class ValidPalindrome_125 {
+    public static void main(String[] args) {
+        String str = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(str));
+    }
+    public static boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            
+            char start = s.charAt(i);
+            char end = s.charAt(j);
+            
+            if (!Character.isLetterOrDigit(start)) {
+                i++;
+                continue;
+            }
+            
+            if (!Character.isLetterOrDigit(end)) {
+                j--;
+                continue;
+            }
+            
+            if (Character.toLowerCase(start) != Character.toLowerCase(end)) {
+                return false;
+            }
+            
+            i++;
+            j--;    
+        }
+    
+    return true;
+    }
+}
