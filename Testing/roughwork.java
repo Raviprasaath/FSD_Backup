1c5b13969d2ff681288d1afc555d5a1f1465a8b4
Raviprasaath <raviprasaath1@gmail.com>
2023-03-22 22:13:34 +0530

    22-03-2023

--- Testing/roughwork.java
+++ Testing/roughwork.java
@@ -1,36 +1,28 @@
 package Testing;
 
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class roughwork {
   	public static void main(String[] args) {
-		int [] array = {4,1,2};
-        int n = array.length;
-        
-        int count = 0;
-        int sumprev = 0;
-        int sumpost = 0;
-        int i =0, j=n-1;
-        while (i < n && j != 0) {
-            sumprev += array[i];
-            if (sumprev < array[j]) {
-                sumpost += array[j];
-            }
-
-            if (sumprev == sumpost) {
-                count++;
-                break;
-            } 
-            if (sumprev < sumpost) {
-                i++;
-            } else {
-                i++;
-                j--;
+		int [] array = {3,1,4,1,5};
+        System.out.println(findPairs(array, 2));
+	}
+    public static int findPairs(int[] nums, int k) {
+        HashMap <Integer, Integer> hmap = new HashMap<>();
+        for (int num : nums) {
+            hmap.put(num, hmap.getOrDefault(num, 0)+1);
+        }
+        int result = 0;
+        for (int x : hmap.keySet()) {
+            if (k > 0 && hmap.containsKey(x + k) || k == 0 && hmap.get(x) > 1) {
+                result ++;
             }
         }
-        System.out.print(count!=0?sumprev:0);   
-	}
-    
+        return result;
+    }
 }
 
 
