8afeabab163849136ba6c44e4daab5c3cc66d02c
Raviprasaath <raviprasaath1@gmail.com>
2023-03-28 22:31:37 +0530

    28-03-2023

--- Testing/roughwork.java
+++ Testing/roughwork.java
@@ -5,34 +5,7 @@ import java.util.Stack;
 
 public class roughwork {
   	public static void main(String[] args) {
-		St.push(1);
-        St.push(2);
-        St.push(3);
-        St.push(4);
-        System.out.println(St);
-        Reverse_Stack();
-        System.out.println("Reversed Stack");
-        System.out.println(St);
         
-	}
-    static Stack <Integer> St = new Stack <Integer>();
-    public static void Reverse_Stack(){
-        if (St.size() > 0) {
-            int x = St.peek();
-            St.pop();
-            Reverse_Stack();
-            pushval(x);
-        }    
-    }
-    public static void pushval(int temp) {
-        if (St.isEmpty()) {
-            St.push(temp);
-        } else {
-            int x = St.peek();
-            St.pop();
-            pushval(temp);
-            St.push(x);
-        }
     }
 }
 
