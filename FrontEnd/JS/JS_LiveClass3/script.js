55b200c6fbc52855bb40a2c01fbee2f82ca8545b
Raviprasaath <raviprasaath1@gmail.com>
2023-04-18 10:53:45 +0530

    18-04-2023

---
+++ FrontEnd/JS/JS_LiveClass3/script.js
@@ -0,0 +1,80 @@
+//17-04-2023
+
+
+// --------------Example 1-----------------------
+// var a = 12;
+// console.log(a)
+
+// function sum (a, b) {
+//     var c = a+b
+//     return c
+// }
+
+// const result = sum(1,2)
+// console.log(result)
+
+
+
+// --------------Example 2-----------------------
+
+let num1 = 12;
+let num2 = 13;
+
+function sum(a , b) {                 //  a, and b are arguments.      
+    let c = a + b;
+    return c;
+}
+
+let result  = sum(num1, num2)
+
+function calc(fn, val) {
+    console.log(fn)
+    console.log(val)
+}
+
+calc(sub, 2)
+
+function sum(a,b) {
+    return a+ b;
+}
+
+function sub(a,b) {
+    return a-b
+}
+
+
+// --------------Example 3-----------------------
+
+// spaceShip(lifeSupport, 'utkarshasdasdasdasdas', 24)
+// function spaceShip(func, num1, num2) {
+
+//     console.log(num1, num2);
+
+//     if(num1 === "utkarsh") {
+//         func()
+//     } else {
+//         console.log('hacking spaceship dectected')
+//     }
+// }
+
+// function weapons() {
+//     console.log('weapons acrtivaged');
+// }
+
+// function lifeSupport() {
+//     console.log('lifeSupport');
+// }
+
+// function food() {
+//     console.log('food');
+// }
+
+
+// function sum(a,b) {
+//     return a + b;
+// }
+
+// sum() // function call.
+
+
+
